88abe5a604502896a6a2c630578f3d061933c31f
Pedro <pedroalabart@hotmail.com>
2025-08-08 11:05:18 -0300

    borre las credenciales del archivo s3_conn

--- util/s3_conn.py
+++ util/s3_conn.py
@@ -7,12 +7,13 @@ load_dotenv()
 aws_access_key_id = os.environ['ACCESS_KEY']
 aws_secret_access_key = os.environ['SECRET_ACCESS_KEY']
 
-print(aws_access_key_id)
-print(aws_secret_access_key)
+
 # Cliente S3 con credenciales
 s3 = boto3.client(
     "s3",
     aws_access_key_id=aws_access_key_id,
     aws_secret_access_key=aws_secret_access_key
 )
-#docker run -e ACCESS_KEY=AKIAXXHL2UEM6HZ4ELBV -e SECRET_ACCESS_KEY=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 henry-2do-pi
+
+
+
