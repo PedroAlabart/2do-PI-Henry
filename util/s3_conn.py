eec04165fb7acc4960586635a6c17741ae747d74
Pedro <pedroalabart@hotmail.com>
2025-08-08 13:28:18 -0300

    delete private aws data

--- util/s3_conn.py
+++ util/s3_conn.py
@@ -7,7 +7,8 @@ load_dotenv()
 aws_access_key_id = os.environ['ACCESS_KEY']
 aws_secret_access_key = os.environ['SECRET_ACCESS_KEY']
 
-
+print(aws_access_key_id)
+print(aws_secret_access_key)
 # Cliente S3 con credenciales
 s3 = boto3.client(
     "s3",
